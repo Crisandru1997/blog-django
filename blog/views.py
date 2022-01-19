055fbf6198d3bd5020105ced67dd96ff26d948d6
Crisandru <crisandru1997@gmail.com>
2022-01-19 10:42:59 -0300

    Agregadas vistas y plantilla para el detalle del post del blog así como también CSS para el sitio.

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,9 @@
+# Importaciones
 from turtle import pos
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.utils import timezone
+
 # Tenemos diferentes piezas en su lugar: el modelo Post está definido en models.py, 
 # tenemos a post_list en views.py y la plantilla agregada. 
 # ¿Pero cómo haremos realmente para que nuestros posts aparezcan 
@@ -14,6 +16,20 @@ from django.utils import timezone
 # En una vista decidimos qué (modelo) se mostrará en una plantilla.
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.all().order_by('published_date')
     # render que reproduce (construye) nuestra plantilla blog/post_list.html.
     return render(request, 'blog/post_list.html', {'posts':posts})
+
+# def post_detail (request, pk): Tenga en cuenta que necesitamos usar exactamente 
+# el mismo nombre que el que especificamos en urls (pk). ¡Omitir esta variable es 
+# incorrecto y resultará en un error!
+def post_detail(request, pk):
+    
+    # No captura el error en caso de que el post no exista.
+    # post = Post.objects.get(pk=pk)
+    
+    # Django tiene una función que se encarga de esto: get_object_or_404
+    # en caso de que no haya ningún post con el pk se mostrara una pagina mas agradable (Page Not Found 404).
+    # Lo bueno es que podemos crear nuestra propia pagina error 404, pero aqui no lo haremos.
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post':post})
